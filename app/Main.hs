ce6b58b62578d47534049f55d741bc6bf09978ec
Tim McIver <tim@timmciver.com>
2025-02-14 20:12:22 -0500

    Factor out a `getEnv` function in Main.

--- app/Main.hs
+++ app/Main.hs
@@ -9,25 +9,29 @@ import Data.Maybe (fromJust)
 import Text.URI (mkURI, URI)
 
 import RainbowHash.LinkedData (putFile)
-import RainbowHash.MediaType (MediaType(..))
+--import RainbowHash.MediaType (MediaType(..))
 import RainbowHash.App (runApp, appErrorToString, Env(..))
 
 main :: IO ()
 main = do
   let file :: FilePath
       file = "/home/tim/workspace/haskell/rainbow-hash-ld/default.nix"
-      -- mediaType = Nothing
-      --mediaType = Just $ MediaType "application/octet-stream" ""
       mediaType = Nothing
-      blobStorageUrl :: URI
-      blobStorageUrl = "http://localhost:3030/blobs" & mkURI & fromJust
-      sparqlEndpoint :: URI
-      sparqlEndpoint = "http://localhost:3031/sparql" & mkURI & fromJust
-      env :: Env
-      env = Env blobStorageUrl sparqlEndpoint
-  --putStrLn $ "Putting file " <> file <> " in store."
+      --mediaType = Just $ MediaType "application/octet-stream" ""
+
+  env <- getEnv
 
   either' <- runApp (putFile file mediaType) env
   case either' of
     Left e -> putStrLn $ appErrorToString e
     Right _ -> pure ()
+
+getEnv :: IO Env
+getEnv = do
+  let blobStorageUrl :: URI
+      blobStorageUrl = "http://localhost:3030/blobs" & mkURI & fromJust
+      sparqlEndpoint :: URI
+      sparqlEndpoint = "http://localhost:3031/sparql" & mkURI & fromJust
+      env :: Env
+      env = Env blobStorageUrl sparqlEndpoint
+  pure env
