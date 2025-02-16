c74aace0abc555936c967ebd2bd03a5d47dc7a52
Tim McIver <tim@timmciver.com>
2025-02-16 11:34:24 -0500

    Add some RDF Schema properties.
    Rearrange the way `triples` is constructred.

--- app/RainbowHash/RDF4H.hs
+++ app/RainbowHash/RDF4H.hs
@@ -40,28 +40,39 @@ fileDataToRDF blobUrl maybeFileName time mt = do
       prefixes :: PrefixMappings
       prefixes = mempty
         & Map.insert "rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        & Map.insert "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
         & Map.insert "nfo" "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#"
         & Map.insert "xsd" "http://www.w3.org/2001/XMLSchema#"
         & Map.insert "schema" "https://schema.org/"
         & PrefixMappings
 
-      triples = [
-                  -- RDF
-                  triple (unode urlText) (unode "rdf:type") (unode "nfo:FileDataObject")
+      triples =
+        -- RDF
+        [triple (unode urlText) (unode "rdf:type") (unode "nfo:FileDataObject")]
+        <>
 
-                -- NEPOMUK File Ontology (nfo)
-                , triple (unode urlText) (unode "nfo:fileUrl") (unode $ render blobUrl)
+        -- RDFS
+        let label = case maybeFileName of
+                      Just fileName -> "A file with name \"" <> fileName <> "\""
+                      Nothing -> "A file with no name"
+        in
+          [ triple (unode urlText) (unode "rdfs:label") (unode $ show label)
+          , triple (unode urlText) (unode "rdfs:comment") (unode $ show label)
+          ]
+        <>
 
-                , triple (unode urlText) (unode "nfo:fileCreated") (unode $ timeISO8601 <> "^^xsd:dateTime")
+        -- NEPOMUK File Ontology (nfo)
+        [ triple (unode urlText) (unode "nfo:fileUrl") (unode $ render blobUrl)
+        , triple (unode urlText) (unode "nfo:fileCreated") (unode $ timeISO8601 <> "^^xsd:dateTime")
+        ]
+        <>
+        case maybeFileName of
+          Just fileName -> [triple (unode urlText) (unode "nfo:fileName") (unode $ show fileName)]
+          Nothing -> []
+        <>
 
-                -- Schema.org stuff
-                , triple (unode urlText) (unode "schema:encodingFormat") (unode $ show $ mediaTypeToText mt)
-                ]
-                <>
-                -- conditional triples
-                case maybeFileName of
-                    Just fileName -> [triple (unode urlText) (unode "nfo:fileName") (unode $ show fileName)]
-                    Nothing -> []
+        -- Schema.org stuff
+        [triple (unode urlText) (unode "schema:encodingFormat") (unode $ show $ mediaTypeToText mt)]
 
       rdf = mkRdf triples (Just $ BaseUrl baseUrlText) prefixes
 
